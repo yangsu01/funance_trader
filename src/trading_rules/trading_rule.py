254c894e0f38d7c901aacbba0c3c822dacbd7d35
yangsu01 <ssgc1101@gmail.com>
2025-02-07 14:05:59 -0800

    refactor: update default methods for Trading Rules

--- src/trading_rules/trading_rule.py
+++ src/trading_rules/trading_rule.py
@@ -4,13 +4,12 @@ import pandas as pd
 
 class TradingRule(ABC):
     @abstractmethod
-    def generate_forecast(self, data: pd.DataFrame) -> float:
+    def generate_next_forecast(self, data: pd.Series) -> float:
         """Generates a forecast for next period based on input data
-            Forecast is a value between -20 and 20 with mean(abs(forecast)) = 10
-            With -20 representing a strong sell signal, 20 a strong buy signal, and 0 being neutral
+            Meant to be used one step at a time for backtesting
 
         Args:
-            data (pd.DataFrame): time series data 
+            data (pd.Series): time series data
 
         Returns:
             float: forecast for next period
@@ -18,15 +17,17 @@ class TradingRule(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def generate_positions(self, forecast: int, cash: float, price: float) -> dict:
-        """Determines the size of the position to take based on forecast, cash, and price
+    def generate_forecasts(self, data: pd.DataFrame) -> pd.Series:
+        """Generates a series of forecasts for historical price data.
+            Forecasts are generated after sufficient data is available.
 
         Args:
-            forecast (int): forecast for next period
-            cash (float): amount of cash available
-            price (float): price of asset
+            data (pd.DataFrame): historical price data with 'Close' column
 
         Returns:
-            int: number of shares to buy (positive integer) or sell (negative integer)
+            pd.Series: series of forecasts
         """
         raise NotImplementedError
+
+    
+
